3cb3d26fe7ee770ab89ed200e996dd5a9144db25
kiowesmith21 <kiowesmith@gmail.com>
2024-02-25 14:25:54 -0500

    add footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import PostPage from './pages/PostPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/post" element={<PostPage />} />
             </Routes> 
         </div> 
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         </ul>
 
         <img src="menu.svg" alt="menu" width={32} height={32}
-        className="inline-block cursor-pointer lg:hidden" />
+        className="inline-block cursor-pointer lg:hidden" /> 
     </nav>
   )
 }
